c80e1c345804b047724c900dbc6855fcf4488b72
Nishant Minerva <nishantcafe@gmail.com>
2024-01-21 14:34:13 +0530

    Initial Commit

---
+++ HelloAppleWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloAppleWatch Watch App
+//
+//  Created by Nishant Minerva on 21/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloAppleWatch Watch App/HelloAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch Watch App
+//
+//  Created by Nishant Minerva on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
